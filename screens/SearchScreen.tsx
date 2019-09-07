9644e8dcd65c0126fc24012c81f29f3f030d5b28
Sergei Chestakov <schestakov@ucdavis.edu>
2019-09-07 16:23:56 -0700

    Add types to response/query and fix getting current pos

--- screens/SearchScreen.tsx
+++ screens/SearchScreen.tsx
@@ -3,77 +3,101 @@ import { StyleSheet, Text, View } from 'react-native';
 import { NavigationScreenProp } from 'react-navigation';
 import { gql } from "apollo-boost";
 
+type Location = {
+  address1: string,
+  city: string,
+  state: string,
+}
+
+type Business = {
+  name: string,
+  rating: number,
+  review_count: number,
+  distance: number,
+  photos: string,
+  url: string,
+  location: Location,
+}
+
 export default class SearchScreen extends React.Component<
   { // props
     navigation: NavigationScreenProp<any,any>
   },
   { // state
-    location: string,
-    longitude: string,
-    latitude: string,
+    results: Business[],
   }> {
   constructor(props) {
     super(props);
 
     this.state = {
-      location: "",
-      longitude: null,
-      latitude: null,
+      results: [],
     }
   }
 
-  componentWillMount() {
-    this.getCurrentPosition();
-  }
+  async componentWillMount() {
+    const { location, longitude, latitude } = await this.getCurrentPosition();
 
-  getCurrentPosition() {
-    navigator.geolocation.getCurrentPosition(
-      position => {
-        const latitude = JSON.stringify(position.coords.latitude);
-        const longitude = JSON.stringify(position.coords.longitude);
-
-        const { navigation } = this.props;
-        const location = navigation.getParam('location', "");
-
-        this.setState({
-          latitude,
-          longitude,
-          location,
-        });
-      }
-    );
-  }
-
-  render() {
     const { navigation } = this.props;
     const client = navigation.getParam('client');
     const value: string = navigation.getParam('value');
 
-    // Either location is empty or long/lat is empty
-    const { location, longitude, latitude } = this.state;
-
-    if (longitude === null) {
-      return (<Text>Loading</Text>)
-    }
+    const locationString = location !== null ?
+      `location: "${location}"` :
+      `longitude: ${longitude}\nlatitude: ${latitude}`
 
     const query = gql`
     {
       search(term: "${value}",
-            location: "${location}",
-            longitude: ${longitude},
-            latitude: ${latitude},
-            limit: 5) {
+            ${locationString}) {
         total
         business {
             name
+            rating
+            review_count
+            distance
+            photos
             url
+            location {
+              address1
+              city
+              state
+            }
         }
       }
     }
-    `
+    `;
     client
     .query({ query })
-    .then(result => console.log(result));
+    .then(results => this.setState({results}));
+  }
+
+  async getCurrentPosition(): Promise<any> {
+    return new Promise((resolve) => {
+      navigator.geolocation.getCurrentPosition(
+        position => {
+          const latitude = JSON.stringify(position.coords.latitude);
+          const longitude = JSON.stringify(position.coords.longitude);
+
+          const { navigation } = this.props;
+          const location = navigation.getParam('location', null);
+
+          resolve({
+            latitude,
+            longitude,
+            location,
+          });
+        }
+      );
+    });
+  }
+
+  render() {
+    const { results } = this.state;
+    if (results.length === 0) {
+      return (
+        <Text>Loading...</Text>
+      )
+    }
 
     return (
       <View style={{ flex: 1, alignItems: 'center', justifyContent: 'center' }}>
