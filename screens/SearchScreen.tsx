9f0d55ae36f845c15365d65a22b0be670fc419c2
Sergei Chestakov <schestakov@ucdavis.edu>
2019-09-07 19:28:48 -0700

    Add Map View for nearby businesses

--- screens/SearchScreen.tsx
+++ screens/SearchScreen.tsx
@@ -1,13 +1,20 @@
 import React from 'react';
-import { StyleSheet, Text, View, ScrollView, Image } from 'react-native';
+import { StyleSheet, Text, View, ScrollView, Image, Dimensions } from 'react-native';
 import { Card, ListItem, Button, Icon } from 'react-native-elements'
 import { NavigationScreenProp } from 'react-navigation';
+import MapView, { Marker } from 'react-native-maps';
 import { gql } from "apollo-boost";
+import { valueToObjectRepresentation } from 'apollo-utilities';
 
 type Location = {
   address1: string,
   city: string,
   state: string,
+}
+
+type Coordinates = {
+  longitude: number,
+  latitude: number,
 }
 
 type Business = {
@@ -18,6 +25,7 @@ type Business = {
   photos: string,
   url: string,
   location: Location,
+  coordinates: Coordinates,
 }
 
 const COLORS = {
@@ -31,6 +39,14 @@ export default class SearchScreen extends React.Component<
   },
   { // state
     results: Business[],
+    region: {
+      latitude: number,
+      longitude: number,
+      latitudeDelta: number,
+      longitudeDelta: number,
+    } | null,
+    showMapView: boolean,
+    isMapReady: boolean,
   }> {
   static navigationOptions = {
     headerStyle: {
@@ -46,13 +62,19 @@ export default class SearchScreen extends React.Component<
 
     this.state = {
       results: [],
+      region: null,
+      showMapView: false,
+      isMapReady: false,
     }
   }
 
   async componentWillMount() {
-    const { location, longitude, latitude } = await this.getCurrentPosition();
+    const { longitude, latitude } = await this.getCurrentPosition();
+
+    this.setState({region: { longitude, latitude, latitudeDelta: 0.0922, longitudeDelta: 0.0421 }})
 
     const { navigation } = this.props;
+    const location = navigation.getParam('location', null);
     const client = navigation.getParam('client');
     const value: string = navigation.getParam('value');
 
@@ -77,6 +99,10 @@ export default class SearchScreen extends React.Component<
               city
               state
             }
+            coordinates {
+              latitude
+              longitude
+            }
         }
       }
     }
@@ -86,24 +112,49 @@ export default class SearchScreen extends React.Component<
     .then(results => this.setState({results: results.data.search.business}));
   }
 
-  async getCurrentPosition(): Promise<any> {
+  async getCurrentPosition(): Promise<Coordinates> {
     return new Promise((resolve) => {
       navigator.geolocation.getCurrentPosition(
         position => {
-          const latitude = JSON.stringify(position.coords.latitude);
-          const longitude = JSON.stringify(position.coords.longitude);
-
-          const { navigation } = this.props;
-          const location = navigation.getParam('location', null);
+          const latitude = Number.parseFloat(JSON.stringify(position.coords.latitude));
+          const longitude = Number.parseFloat(JSON.stringify(position.coords.longitude));
 
           resolve({
             latitude,
-            longitude,
-            location,
+            longitude
           });
         }
       );
     });
+  }
+
+  onMapLayout = () => {
+    this.setState({ isMapReady: true });
+  }
+
+  renderMap() {
+    if (!this.state.region) {
+      return (
+        <Text>Loading...</Text>
+      )
+    }
+    
+    return (
+      <MapView
+        region={this.state.region}
+        style={styles.map}
+        onLayout={this.onMapLayout}
+      >
+        {this.state.isMapReady && this.state.results.map((result, index) => (
+          <Marker
+            coordinate={result.coordinates}
+            key={index}
+            title={result.name}
+            description={"Delicious food here."}
+          />
+        ))}
+      </ MapView>
+    )
   }
 
   createStarIcons(rating: number, review_count: number) {
@@ -142,10 +193,12 @@ export default class SearchScreen extends React.Component<
         <Text>Loading...</Text>
       )
     }
-    console.log(results)
 
     return (
       <ScrollView style={{ flex: 1 }}>
+        <View style={styles.map}>
+          {this.renderMap()}
+        </View>
         {
           results.map((result, index) => {
             const title = `${index + 1}. ${result.name}`;
@@ -179,5 +232,10 @@ export default class SearchScreen extends React.Component<
 const styles = StyleSheet.create({
   title: {
     textAlign: 'left',
+  },
+  map: {
+    flex: 1,
+    width: Dimensions.get('window').width,
+    height: Dimensions.get('window').height,
   }
 });
